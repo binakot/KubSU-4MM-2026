660849feb1cbc6ab941a356e77d1f177943c38a3
Ivan Muratov <binakot@gmail.com>
2026-02-22 14:59:49 +0300

    MVP

--- extension/content-script.js
+++ extension/content-script.js
@@ -1,7 +1,20 @@
-//console.log('Hello, World!');
-//alert('Hello, World!');
+const MAX_TEXT_LENGTH = 1_000;
 
-//const pageTitle = document.title;
-//console.log("Заголовок страницы:", pageTitle);
+function parseTextContent(maxLen) {
+  if (!document.body?.innerText) {
+    return "";
+  }
+  return document.body.innerText.trim().slice(0, maxLen);
+}
 
-chrome.runtime.sendMessage({action: "sendTitle", title: document.title});
+window.addEventListener('load', (event) => {
+  const payload = {
+    type: "view",
+    url: location.href,
+    title: document.title || "",
+    lang: document.documentElement?.lang || "",
+    text: parseTextContent(MAX_TEXT_LENGTH)
+  };
+
+  chrome.runtime.sendMessage(payload);
+});
--- extension/service-worker.js
+++ extension/service-worker.js
@@ -1,5 +1,27 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "sendTitle") {
-        console.log("Получен заголовок от контент-скрипта:", request.title);
-    }
+const IGNORE_URL_PREFIXES = [
+  "chrome://"
+];
+
+function buildPayload(message) {
+  return {
+    url: message.url,
+    title: message.title,
+    lang: message.lang,
+    text: message.text,
+    timestamp: new Date().toISOString()
+  };
+}
+
+chrome.runtime.onMessage.addListener(async (message) => {
+  console.log("Received message", message);
+
+  if (!message || message.type !== "view") {
+    return; // Пустое или неподходящее сообщение
+  }
+  if (!message.url || IGNORE_URL_PREFIXES.some((prefix) => message.url.startsWith(prefix))) {
+    return; // Пустой или неподходящий URL
+  }
+
+  const payload = buildPayload(message);
+  console.log("Sending payload", payload); // TODO Send to Backend API
 });
