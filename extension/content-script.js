5e32a705403abbf272996283e5950afbedd55846
Ivan Muratov <binakot@gmail.com>
2026-03-16 17:14:55 +0300

    RC1

--- extension/content-script.js
+++ extension/content-script.js
@@ -1,10 +1,18 @@
 const MAX_TEXT_LENGTH = 1_000;
 
-function parseTextContent(maxLen) {
+function getBodyText(maxLen) {
   if (!document.body?.innerText) {
     return "";
   }
   return document.body.innerText.trim().slice(0, maxLen);
+}
+
+function getTextBySelectors(selectors) {
+  const headers = [];
+  document.querySelectorAll(selectors).forEach((header) => {
+    headers.push(header.innerText.trim());
+  });
+  return headers.join(".");
 }
 
 window.addEventListener('load', (event) => {
@@ -13,7 +21,8 @@ window.addEventListener('load', (event) => {
     url: location.href,
     title: document.title || "",
     lang: document.documentElement?.lang || "",
-    text: parseTextContent(MAX_TEXT_LENGTH)
+    text: getBodyText(MAX_TEXT_LENGTH),
+    headers: getTextBySelectors("h1, h2, h3")
   };
 
   chrome.runtime.sendMessage(payload);
